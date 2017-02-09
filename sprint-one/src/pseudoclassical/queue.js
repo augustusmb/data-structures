25c79842320cc023cb42441d5a43f996e85c0e66
augustusmb <augustusmb@gmail.com>
2017-02-09 11:10:44 -0800

    Finsih all implementations

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.total = 0;
+  this.counter = 0; 
+  this.firstInLine = 0;
 };
 
+Queue.prototype = {
+  enqueue: function(value) {
+    this[this.counter] = value;
+    this.counter++;
+    this.total++;
+  },
+  dequeue: function() {
+    if (this.total > 0) {
+      this.total--;
+    }
+    var dequeued = this[this.firstInLine];
+    this.firstInLine++;
+    return dequeued;
+    
+  },
+  size: function() {
+    return this.total;
+  },
+  constructor: Stack
+};
 
+// var obj = new Stack();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.total = 0;
+  this.lastIn;
+  this.penultimate;
 };
+
+Stack.prototype = {
+  push: function(value) {
+    this.total++;
+    this.penultimate = this.lastIn;
+    this.lastIn = value;
+  }, 
+  pop: function() {
+    if (this.total > 0) {
+      this.total--;
+    }
+    var popped = this.lastIn;
+    this.lastIn = this.penultimate;
+    return popped;
+  }, 
+  size: function() {
+    return this.total;
+  },
+  constructor: Stack
+};
+
+// var obj = new Stack();
 
 
