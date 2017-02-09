bee1c9d8634d99da78c8ea0037f197a7daa57778
augustusmb <augustusmb@gmail.com>
2017-02-08 16:25:45 -0800

    Pass all functional-shared tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.counter = 0;
+  obj.lastIn;
+  obj.penultimate;
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this.counter++;
+    this.penultimate = this.lastIn;
+    this.lastIn = value;
+  }, 
+  pop: function() {
+    this.counter--;
+    var popped = this.lastIn;
+    this.lastIn = this.penultimate;
+    return popped;
+  }, 
+  size: function() {
+    return this.counter < 0 ? 0 : this.counter;
+  }
+};
