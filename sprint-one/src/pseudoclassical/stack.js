66a001a133b8430f555e02c8b866e80e4f1de89b
augustusmb <augustusmb@gmail.com>
2017-02-09 11:38:09 -0800

    Correct small mistake

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -6,6 +6,34 @@ var Stack = function() {
   this.penultimate;
 };
 
+Stack.prototype.push = function(value) {
+  this.total++;
+  this.penultimate = this.lastIn;
+  this.lastIn = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.total > 0) {
+    this.total--;
+  }
+  var popped = this.lastIn;
+  this.lastIn = this.penultimate;
+  return popped;
+}; 
+
+Stack.prototype.size = function() {
+  return this.total;
+};
+
+
+/*var Stack = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  this.total = 0;
+  this.lastIn;
+  this.penultimate;
+};
+
 Stack.prototype = {
   push: function(value) {
     this.total++;
@@ -24,8 +52,4 @@ Stack.prototype = {
     return this.total;
   },
   constructor: Stack
-};
-
-// var obj = new Stack();
-
-
+};*/
