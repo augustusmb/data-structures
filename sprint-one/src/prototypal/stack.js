e64cec8d367e70d744a6c4d306b97c1064637aa9
augustusmb <augustusmb@gmail.com>
2017-02-08 20:06:29 -0800

    Complete stack prototypal implementation

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
+var stackMethods = {
+  push: function(value) {
+    this.total++;
+    this.penultimate = this.lastIn;
+    this.lastIn = value;
+  },
+  pop: function() {
+    this.total--;
+    var popped = this.lastIn;
+    this.lastIn = this.penultimate;
+    return popped;
+  },
+  size: function() {
+    return this.total < 0 ? 0 : this.total;
+  }
+};
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.total = 0;
+  obj.lastIn;
+  obj.penultimate;
+  return obj;
 };
-
-var stackMethods = {};
-
-
