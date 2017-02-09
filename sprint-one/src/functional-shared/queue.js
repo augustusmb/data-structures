670d2ec34114a68c1a8b5b885efa0f153a13ac4f
augustusmb <augustusmb@gmail.com>
2017-02-08 19:53:50 -0800

    Complete tests through functional shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -4,16 +4,21 @@ var Queue = function() {
   var obj = {};
   obj.counter = 0;
   obj.total = 0;
+  obj.firstInLine = 0;
   _.extend(obj, queueMethods);
   return obj;
 };
 var queueMethods = {
   enqueue: function(value) {
+    this[this.counter] = value;
     this.counter++;
     this.total++;
   },
   dequeue: function() {
     this.total--;
+    var dequeued = this[this.firstInLine];
+    this.firstInLine++;
+    return dequeued; 
   },
   size: function() {
     return this.total < 0 ? 0 : this.total;
