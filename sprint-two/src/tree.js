24046dce2475f9723b1b7c60c0cdc07c86d65497
augustusmb <augustusmb@gmail.com>
2017-02-10 14:12:42 -0800

    Implement recursive call on .contains

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,27 +1,37 @@
 var Tree = function(value) {
   var newTree = {};
-  newTree.value = value;
   _.extend(newTree, treeMethods);
-  // your code here
-  newTree.children = [];  // fix me
-
-
+  newTree.value = value;
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  //newTree.children = value;
-  this.children.push(value);
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
-  return this.children.includes(target);
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
 
-
-
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? 
+ addChild is constant.  contains is linear
  */
+
+
+
+
+// var tree5 = Tree(value);
+
+// tree5.addChild(7);
