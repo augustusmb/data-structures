dd9d18adf3e7a039a84e5338d415af5d06f92f70
augustusmb <augustusmb@gmail.com>
2017-02-09 20:16:30 -0800

    Made our children object an array, pass values into that array with addChild

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
 
   return newTree;
 };
@@ -11,9 +12,12 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //newTree.children = value;
+  this.children.push(value);
 };
 
 treeMethods.contains = function(target) {
+  return this.children.includes(target);
 };
 
 
