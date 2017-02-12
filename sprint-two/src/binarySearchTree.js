3fedc5529f0cc17e17dfc01da18481ceed67913b
augustusmb <augustusmb@gmail.com>
2017-02-11 16:23:46 -0800

    Start binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,26 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  // if value === this.value
+  if ()
+    // do nothing
+  // else if value > this.value
+   // set right property to value
+  // else
+    // set left property to value
+};
 
+BinarySearchTree.prototype.contains = function() {
+  
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+  
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
